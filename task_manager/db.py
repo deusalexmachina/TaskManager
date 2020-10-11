e1c66b2b1c9d167d6cb5aab0a7f8c0fda9c5abb6
Alex Hong <ah4927@nyu.edu>
2020-10-11 16:44:45 -0400

    create_app() factory, init_db(), and basic schema

--- task_manager/db.py
+++ task_manager/db.py
@@ -4,12 +4,52 @@ At first, it will just contain stubs that return fake data.
 Gradually, we will fill in actual calls to our datastore.
 """
 
-#from flask_pymongo import pymongo
+import sqlite3
 
-#CONNECTION_STRING=
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
+
 
 taskid = 1
 userid = 1
+
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables"""
+    init_db()
+    click.echo("Initialized the database.")
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
 
 
 def fetch_tasks():
